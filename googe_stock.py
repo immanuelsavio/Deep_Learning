773a8a5110365f0b525c2a5516f53586144d33b4
immanuelsavio <immanuelsavio@github.com>
2018-12-14 17:46:29 +0530

    Update googe_stock.py

--- googe_stock.py
+++ googe_stock.py
@@ -2,7 +2,8 @@ import pandas as pd
 import quandl 
 import math
 import numpy as np 
-from sklearn import preprocessing, cross_validation,
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 #print(df.head())
@@ -17,4 +18,17 @@ df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.1*len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
-print(df.tail())
+#print(df.tail())
+
+X = np.array(df.drop(['Label'],1))
+y = np.array(['Label'])
+
+X = preprocessing.scale(X)
+#X = X[:-forecast_out+1]
+df.dropna(inplace=True)
+y = np.array(df['Label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size = 0.2)
+
+clf = LinearRegression()
+clf.fit(X_train,y_train)
