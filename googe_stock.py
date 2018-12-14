fc1f437be2fa6b6236b5f3dde0286a7d4b110809
immanuelsavio <immanuelsavio@github.com>
2018-12-14 14:24:36 +0530

    Update googe_stock.py

--- googe_stock.py
+++ googe_stock.py
@@ -5,3 +5,7 @@ df = quandl.get('WIKI/GOOGL')
 #print(df.head())
 df = df [['Adj. Open','Adj. High','Adj. Close','Adj. Volume',]]
 df['HL_PCT'] = (df['Adj. High']-df['Adj. Close'])/df['Adj. Close'] * 100
+df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open'] * 100
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
+
+print(df.head())
