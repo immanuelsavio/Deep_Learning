8824566dd7fe2244837de09c39defafae6868f2b
immanuelsavio <immanuelsavio@github.com>
2018-12-15 14:56:54 +0530

    Update Linear_Regression.py

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
-def best_fit_slope(xs,ys):
-    m = (mean(xs) * mean(ys))
-    return m     
+def best_fit_slope_and_intercept(xs,ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs*ys)) / (mean(xs)**2 - mean(xs**2)))
+    return m, b    
 
-m = best_fit_slope(xs,ys)
+m, b = best_fit_slope_and_intercept(xs,ys)
+print(m)
