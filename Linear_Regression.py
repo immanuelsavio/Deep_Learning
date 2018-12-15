5041d1af0350353fbcffc762c5320312f430d6c1
immanuelsavio <immanuelsavio@github.com>
2018-12-15 14:51:09 +0530

    Update Linear_Regression.py

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -2,8 +2,11 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 
-xs = np.array[1,2,3,4,5,6]
-ys = np.array[5,4,6,5,6,7]
+xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
-plt.scatter(xs,ys)
-plt.show()
+def best_fit_slope(xs,ys):
+    m = (mean(xs) * mean(ys))
+    return m     
+
+m = best_fit_slope(xs,ys)
