20f4cc0e08a2c9dc336d31d6b76860c146de33cf
immanuelsavio <immanuelsavio@github.com>
2018-12-16 17:23:22 +0530

    done and dusted

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,7 +1,9 @@
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
 
+style.use('fivethirtyeight')
 
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,5,6,7], dtype=np.float64)
