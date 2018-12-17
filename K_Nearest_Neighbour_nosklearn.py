bf46401632582954ec88ad4b1a746110ff5f9412
immanuelsavio <immanuelsavio@gmail.com>
2018-12-17 23:35:03 +0530

    Update K_Nearest_Neighbour_nosklearn.py

--- K_Nearest_Neighbour_nosklearn.py
+++ K_Nearest_Neighbour_nosklearn.py
@@ -1,7 +1,12 @@
 from math import sqrt
+import numpy as np
+import warnings
+import matplotlib.pyplot as plt
+from matplotlib import style
+from collections import Counter
+style.use('fivethirtyeight')
 
-plot1 = [1,3]
-plot2 = [2,5]
+dataset = {'k' : [[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]] }
+new_feature = [5,7]
 
-eucl_dist = sqrt( (plot1[0] - plot2[0])**2 + (plot1[1] - plot2[1])**2 )
-print(eucl_dist)
+plt.scatter
