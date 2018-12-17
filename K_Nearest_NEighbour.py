7cd9fe66d44ec0e67495285be60816efaf82c008
immanuelsavio <immanuelsavio@gmail.com>
2018-12-17 19:33:38 +0530

    Update K_Nearest_NEighbour.py

--- K_Nearest_NEighbour.py
+++ K_Nearest_NEighbour.py
@@ -5,3 +5,6 @@ import pandas as pd
 df = pd.read_csv('breast-cancer-wisconsin.data')
 df.replace('?', -99999, inplace =True)
 df.drop(['id'],1,inplace = True)
+
+X = np.array(df.drop(['class']))
+y = np.array(df['class'])
