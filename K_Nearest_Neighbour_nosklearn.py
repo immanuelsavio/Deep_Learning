1926e92ca188735a743f92006a71761ea46da871
immanuelsavio <immanuelsavio@gmail.com>
2018-12-17 23:44:14 +0530

    Update K_Nearest_Neighbour_nosklearn.py

--- K_Nearest_Neighbour_nosklearn.py
+++ K_Nearest_Neighbour_nosklearn.py
@@ -9,4 +9,7 @@ style.use('fivethirtyeight')
 dataset = {'k' : [[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]] }
 new_feature = [5,7]
 
-plt.scatter
+for i in dataset:
+    for ii in dataset[i]:
+        [plt.scatter(ii[0],ii[1], s=100, color=i)]
+plt.show()
