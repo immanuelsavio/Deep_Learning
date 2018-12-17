9880b7bcd89e0de8775f143ed752b9ad0ca4879c
immanuelsavio <immanuelsavio@gmail.com>
2018-12-17 21:35:08 +0530

    Update K_Nearest_NEighbour_sklearn.py

--- K_Nearest_NEighbour_sklearn.py
+++ K_Nearest_NEighbour_sklearn.py
@@ -6,7 +6,7 @@ df = pd.read_csv('breast-cancer-wisconsin.data.txt')
 df.replace('?', -99999, inplace =True)
 df.drop(['id'],1,inplace = True)
 
-X = np.array(df.drop(['class']))
+X = np.array(df.drop(['class'],1))
 y = np.array(df['class'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
@@ -15,3 +15,8 @@ clf.fit(X_train, y_train)
 
 accuracy = clf.score(X_test, y_test)
 print(accuracy)
+
+example_prediction = np.array([4,2,1,1,1,2,3,2,2])
+
+prediction = clf.predict(example_prediction)
+print(prediction)
