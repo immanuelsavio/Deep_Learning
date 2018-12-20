14724051fff6aab3e0d34a4fda85237e55fd386b
immanuelsavio <immanuelsavio@gmail.com>
2018-12-21 01:06:49 +0530

    Update Neural_Netowrk.py

--- Neural Network/Neural_Netowrk.py
+++ Neural Network/Neural_Netowrk.py
@@ -35,4 +35,6 @@ def neural_network_model(data):
     l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
     l3 = tf.nn.relu(l3)
 
+    output = tf.matmul(l2, output_layer['weights']) + output_layer['biases']
+
 
