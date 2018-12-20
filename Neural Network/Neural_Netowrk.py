6e1416a0b589e9a0c989bd93893b82bff28cfc3a
immanuelsavio <immanuelsavio@gmail.com>
2018-12-21 00:51:38 +0530

    Update Neural_Netowrk.py

--- Neural Network/Neural_Netowrk.py
+++ Neural Network/Neural_Netowrk.py
@@ -10,6 +10,18 @@ n_nodes_hl3 = 500
 n_classes =  10
 batch_size = 100
 
-x = tf.placeholder()
-y = tf.placeholder()
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float')
 
+def neural_network_model(data):
+    hidden_1_layer  = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+                       'biases' :tf.Variable(tf.random_normal(n_nodes_hl1))}
+
+    hidden_2_layer  = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                       'biases' :tf.Variable(tf.random_normal(n_nodes_hl2))}
+
+    hidden_3_layer  = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                       'biases' :tf.Variable(tf.random_normal(n_nodes_hl3))}
+
+    output_layer  = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                       'biases' :tf.Variable(tf.random_normal(n_classes))}
