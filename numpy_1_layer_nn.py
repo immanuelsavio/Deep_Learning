bca591ed0b401c5ae98cd8246b6dfde996286127
immanuelsavio <immanuelsavio@gmail.com>
2018-12-26 19:17:56 +0530

    Update numpy_1_layer_nn.py

--- numpy_1_layer_nn.py
+++ numpy_1_layer_nn.py
@@ -1,3 +1,19 @@
+from numpy import exp, array, random, dot
+
+class NeuralNetwork():
+    def __init__(self):
+        
+        random.seed(1)
+
+        self.synaptic_weights = 2 * random.random((3,1)) - 1
+
+
+
+
+
+
+
+
 if __name__ = '__main__':
 
     neural_network = NeuralNetwork()
@@ -7,3 +23,10 @@ if __name__ = '__main__':
 
     training_set_inputs = array([[1,0,1],[1,1,0],[1,0,0],[1,1,1]])
     training_set_outputs = array([[1,1,0,0]]).T
+
+    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+
+    print(" New synaptic weights after training : ",)
+    print(neural_network.synaptic_weights)
+
+    print('')
